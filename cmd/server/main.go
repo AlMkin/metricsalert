ff84b02a3700438d644240d636d50fc910789720
almkin <smukhin1993@gmail.com>
2023-11-07 06:30:40 +0700

    feat(ink3): the fought increment is completed

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,20 +5,18 @@ import (
 	"github.com/AlMkin/metricsalert/internal/handlers"
 	"github.com/AlMkin/metricsalert/internal/server"
 	"github.com/AlMkin/metricsalert/internal/storage"
-	"log"
 )
 
 func main() {
 	var addr string
-	flag.StringVar(&addr, "a", "http://localhost:8080", "Address to listen on")
+	flag.StringVar(&addr, "a", ":8080", "Address to listen on")
 	flag.Parse()
 
 	store := storage.NewMemStorage()
 	handlers.SetRepository(store)
 
 	srv := server.NewServer()
-	log.Printf("Server is starting at %s\n", addr)
 	if err := srv.Run(addr); err != nil {
-		log.Fatalf("Error when running server: %s", err)
+		panic(err)
 	}
 }
