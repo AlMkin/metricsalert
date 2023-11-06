12b4e95724eaeb2d3c2ddc9e4ca86080c57e43ee
almkin <smukhin1993@gmail.com>
2023-11-07 05:44:03 +0700

    feat(ink3): the fought increment is completed

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,22 +2,19 @@ package server
 
 import (
 	"github.com/AlMkin/metricsalert/internal/handlers"
-	"github.com/gorilla/mux"
 	"net/http"
 )
 
 type Server struct {
-	router *mux.Router
+	mux *http.ServeMux
 }
 
 func NewServer() *Server {
-	router := mux.NewRouter()
-	return &Server{router: router}
+	mux := http.NewServeMux()
+	return &Server{mux: mux}
 }
 
-func (s *Server) Run(addr string) error {
-	s.router.HandleFunc("/update/{type}/{name}/{value}", handlers.UpdateMetricsHandler).Methods(http.MethodPost)
-	s.router.HandleFunc("/value/{type}/{name}", handlers.GetMetricsHandler).Methods(http.MethodGet)
-	s.router.HandleFunc("/", handlers.ListMetricsHandler).Methods(http.MethodGet)
-	return http.ListenAndServe(addr, s.router)
+func (s *Server) Run(port string) error {
+	s.mux.HandleFunc("/update/", handlers.UpdateMetricsHandler)
+	return http.ListenAndServe(port, s.mux)
 }
